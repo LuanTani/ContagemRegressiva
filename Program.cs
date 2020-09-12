1e96d9b21f41d0b9f18eed54b7cedd4f22c166dd
unknown <luanpaivatani@gmail.com>
2020-09-11 22:55:08 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preparar...");
+            Console.WriteLine("Pressione ENTER para Continuar");
+            Console.ReadKey();
+            Console.Write("\n");
+
+            Console.WriteLine("Apontar...");
+            Console.WriteLine("Pressione ENTER para Continuar");
+            Console.ReadKey();
+            Console.Write("\n");
+
+            Console.WriteLine("FOGO!");
+            Console.WriteLine("Pressione ENTER para Sair");
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Faça um programa que exiba uma contagem regressiva no seguinte formato:
 Preparar...
 Apontar...
 FOGO!
+
+**Como Executar**
+
+Acesse a pasta do projeto e digite:
+
+```
+dotnet run
+```
